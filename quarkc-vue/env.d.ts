f7715e127a5f023cdc3f067fe921cd19e0655d4c
yangshijun <yangshijun777@gmail.com>
2023-07-15 15:18:01 +0800

    feat: 引入and-design-vue

--- quarkc-vue/env.d.ts
+++ quarkc-vue/env.d.ts
@@ -4,9 +4,17 @@
 declare type Recordable<T = any> = Record<string, T>
 interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string
+  readonly VITE_PUBLIC_PATH: string
+  readonly VITE_DROP_CONSOLE: boolean
+  readonly VITE_OPEN: boolean
   // 更多环境变量...
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
+}
+declare module '*.vue' {
+  import { defineComponent } from 'vue'
+  const Component: ReturnType<typeof defineComponent>
+  export default Component
 }
--- quarkc-vue/src/App.vue
+++ quarkc-vue/src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
+      <a-button>andDesignButton</a-button>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
