0f6e652e3d2259aa6cbe8e3ad183b39443a5c63b
yangshijun <yangshijun777@gmail.com>
2023-08-10 22:27:05 +0800

    update: login页面

--- quarkc-vue/src/views/login/components/loginForm.vue
+++ quarkc-vue/src/views/login/components/loginForm.vue
@@ -2,7 +2,7 @@
   <div class="login-form">
     <div class="login-form-welcome margin-b-20">Welcome Back！\(@^0^@)/</div>
     <div class="login-form-motto margin-b-40">不积跬步，无以至千里；不积小流，无以成江海。🔔</div>
-    <div class="login-form-box margin-b-40">
+    <div class="login-form-box margin-b-20">
       <a-form
         :model="loginForm"
         name="basic"
@@ -33,16 +33,58 @@
         </div>
       </a-form>
     </div>
+    <div class="login-form-remember margin-b-20">
+      <a-radio v-model:checked="loginForm.remember">Remember Me</a-radio>
+      <div>Forget Password</div>
+    </div>
+    <div class="login-form-foot margin-b-20">
+      <a-button class="margin-r-40" shape="round" type="primary">Login Now</a-button>
+      <a-button shape="round" type="primary" class="white-button">Create Account</a-button>
+    </div>
+    <div class="login-form-other">
+      <p>Or you can join with</p>
+      <div class="login-form-other-icons">
+        <img
+          v-for="item of icons"
+          :class="{ large: item.id === 3 }"
+          :key="item.id"
+          :src="getImageUrl(item.img)"
+          alt=""
+          srcset=""
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { reactive } from 'vue'
+const icons = [
+  {
+    id: 1,
+    label: 'qq',
+    img: 'assets/login/qq.svg'
+  },
+  {
+    id: 2,
+    label: 'google',
+    img: 'assets/login/google.svg'
+  },
+  {
+    id: 3,
+    label: 'twitter',
+    img: 'assets/login/twitter.svg'
+  }
+]
 let loginForm = reactive({
   username: '',
   password: '',
   remember: false
 })
+const getImageUrl = (url: string) => {
+  console.log('处理图片路径', import.meta.url)
+  return new URL(`../../../${url}`, import.meta.url).href
+}
 </script>
 
 <style scoped lang="scss">
@@ -112,6 +154,36 @@ let loginForm = reactive({
   }
   &-password {
     background-color: white;
+  }
+  &-remember {
+    width: 100%;
+    padding: 0 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  &-foot {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    .white-button {
+      background-color: #fff;
+      color: #1e1e1e;
+    }
+  }
+  &-other-icons {
+    display: flex;
+    align-items: center;
+    img {
+      margin-right: 16px;
+      width: 32px;
+      height: 32px;
+      cursor: pointer;
+    }
+    .large {
+      width: 40px;
+      height: 40px;
+    }
   }
 }
 </style>
